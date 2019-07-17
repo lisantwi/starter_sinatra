15e2ab4e5037cd30c9acf41faf3516c79a25bedc
Trevahr Robertson <Trevahr@gmail.com>
2019-07-17 15:03:37 -0400

    added login screen

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -3,5 +3,23 @@ class OwnersController < Sinatra::Base
     set :views, "app/views/owners"
     set :method_override, true
 
-    
+
+    get "/owners" do
+        @owners = Owner.all
+        erb :show
+    end
+
+    post "/owners" do
+        @owner = Owner.find_or_create_by(name: params["owner_name"])
+        id = @owner.id
+        redirect "owners/#{id}"
+    end
+
+    get "/owners/:id" do
+        id = params[:id]
+        @owner = Owner.find(id)
+        erb :profile
+    end
+
+
 end
